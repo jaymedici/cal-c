d3ca603b18d0f46362e97eff0d9126de0bd598b4
Jay Medici <mabokojohn3@gmail.com>
2022-04-01 10:00:34 +0300

    Added a Test for Site management tasks

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,14 +3,8 @@
 namespace App\Http\Controllers;
 use App\Project;
 use App\Models\User;
-use App\Department;
-use App\UserProject;
 use App\Models\Site;
-use App\Models\ProjectSite;
-use Yajra\Datatables\Datatables;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Adldap\Laravel\Facades\Adldap;
 use App\Http\Requests\Projects\StoreProjectRequest;
 use App\Services\ProjectService;
 
@@ -28,60 +22,19 @@ class ProjectsController extends Controller
         $this->projectService = $projectService;
     }
 
-    public function alpineTest()
-    {
-        $users = User::all();
-        $sites = Site::all();
-
-        return view('alpineTest2', compact('users', 'sites'));
-    }
-
-    public function livewireTest()
-    {
-
-        return view('livewireTest');
-    }
-
     public function index(Request $request)
     {
-        if (Auth::check())
-        {
-            $allProjects = Project::paginate(10);
+        $allProjects = Project::paginate(10);
 
-            return view('projects.index', compact('allProjects'));
-        }
-
-        return view('auth.login');
+        return view('projects.index', compact('allProjects'));
     }
-
-    public function projectListdt()
-    {
-        if (Auth::check()){
-            return Datatables::of(UserProject::with('project')->where('user_id',auth::user()->id)->orderBy('project_id'))
-            ->addColumn('editLink', function ($row) {
-                return '<a href="/projectData/'.$row->project_id.'">'."View Data".'</a>';
-            })
-            ->rawColumns(['editLink'])
-            ->make(true);
-        
-    }
-    return view('auth.login');
-    }
-
-
-    
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
         $users = User::all();
         $sites = Site::all();
         
-        return view('projects.create', compact('users', 'sites'));
-        
+        return view('projects.create', compact('users', 'sites'));   
     }
 
     public function store(StoreProjectRequest $request)
@@ -97,75 +50,5 @@ class ProjectsController extends Controller
 
         return redirect()->route('projects.index')->with('success','Project Information has been saved Successfully.');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Project $project)
-    {
-        if (Auth::check()){
-            $projects  = Project::with('pi')->with('pis')->with('dept')->find($project->id);
-            $user = User::all();
-            $department = Department::all();
-             return view('projects.edit',compact('projects','user','department'));
-             }
-             return view('auth.login');
-    }
-
-
-    
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Project $project)
-    {
-        if (Auth::check()){
-
-        $projects=Project::where('id', $project->id)
-                              ->update([
-                                'name'=>$request->input('name'),
-                                'description'=>$request->input('description'),
-                                'updated_by'=>auth::user()->email,
-                                       ]);
-
-if ($projects){
-
-    return back()->withinput()->with('success','Information have been saved Successfully.');
-}
-
-        //redirect
-       // return back()->withinput();
-       return back()->withinput()->with('errors','Error Updating');
-    }
-    return view('auth.login');
-}
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        
-    }
+  
 }
---
+++ tests/Feature/Sites/SiteManagementTests.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Tests\Feature\Sites;
+
+use App\Models\Site;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SiteManagementTests extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+    //php artisan test tests/Feature/Sites/SiteManagementTests.php//
+   
+    public function test_guests_can_not_access_any_of_the_site_routes()
+    {
+        $attributes = Site::factory()->raw();
+
+        $this->get('/sites')->assertRedirect('login');
+
+        $this->get('/sites/create')->assertRedirect('login');
+
+        $this->post('/sites', $attributes)->assertRedirect(('login'));
+    }
+
+    public function test_authenticated_users_can_view_site_pages()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $this->get('sites')->assertStatus(200);
+
+        $this->get('/sites/create')->assertStatus(200);
+    }
+
+    public function test_saved_users_are_loaded_with_the_create_site_page()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $user = User::factory()->create();
+
+        $this->get('/sites/create')->assertSee([$user['name']]);
+    }
+
+    public function test_authenticated_user_can_create_a_site()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs(User::factory()->create());
+
+        $attributes = Site::factory()->raw();
+        $attributes['site_users'] = [1,2,3];
+
+        $this->post('/sites', $attributes)->assertRedirect('/sites');
+    }
+
+    public function test_site_requires_a_site_name()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $attributes = Site::factory()->raw(['site_name' => null]);
+        $attributes['site_users'] = [1,2,3];
+
+        $this->post('/sites', $attributes)->assertSessionHasErrors('site_name');
+    }
+
+    public function test_site_requires_site_users()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $attributes = Site::factory()->raw();
+
+        $this->post('/sites', $attributes)->assertSessionHasErrors('site_users');
+    }
+
+    public function test_site_with_duplicate_site_name_can_not_be_created()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $siteOne = Site::factory()->create();
+
+        $siteTwo = Site::factory()->raw(['site_name' => $siteOne->site_name]);
+        $siteTwo['site_users'] = [1,2,3];
+
+        $this->post('/sites', $siteTwo)->assertSessionHas('error_message');
+    }
+
+    public function test_updated_by_is_added_on_site_creation()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $attributes = Site::factory()->raw();
+        $attributes['site_users'] = [1,2];
+
+        $this->post('/sites', $attributes);
+        $this->assertDatabaseHas('sites', ['updated_by' => $user->username]);
+    }
+
+    public function test_database_stores_site_users_when_site_is_created()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs(User::factory()->create());
+
+        $attributes = Site::factory()->raw();
+        $attributes['site_users'] = [1,2];
+
+        $this->post('/sites', $attributes);
+
+        $this->assertDatabaseHas('user_sites', ['user_id' => 2]);
+    }
+
+    //Check that Selected Users actuallty exist
+
+}
